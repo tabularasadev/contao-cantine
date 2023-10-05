54661a382b23635cc7feecdcf56460d0a5a55448
Jimmy <jimmy@tabularasa.fr>
2023-10-05 09:51:37 +0200

    Changement enregistrement

--- src/Module/beModuleSaisie.php
+++ src/Module/beModuleSaisie.php
@@ -2,9 +2,12 @@
 
 namespace trdev\ContaoCantineBundle\Module;
 
+use Contao\CoreBundle\Monolog\ContaoContext;
 use Contao\Input;
 use Contao\RequestToken;
+use Contao\System;
 use DateTime;
+use Psr\Log\LogLevel;
 use trdev\ContaoCantineBundle\Model\classeModel;
 use trdev\ContaoCantineBundle\Model\enfantModel;
 use trdev\ContaoCantineBundle\Model\etablissementModel;
@@ -33,28 +36,59 @@ class beModuleSaisie extends \BackendModule
         if (null != Input::post('FORM_SUBMIT') && Input::post('FORM_SUBMIT') == 'saisie_repas') {
             $enfants = Input::post('enfants');
             $date    = strtotime(Input::post('date'));
+            $maj     = [];
             if (null != $enfants && Count($enfants) > 0) {
                 foreach ($enfants as $unEnfant) {
-                    $repas = repasModel::findBy(array('nomEnfant=?', 'date=?'), array($unEnfant, $date));
-                    if (!$repas) {
-                        $repas            = new repasModel();
-                        $repas->tstamp    = time();
-                        $repas->date      = $date;
-                        $repas->nomEnfant = $unEnfant;
-                    }
+                    $enf   = enfantModel::findByPk($unEnfant);
+                    $choix = [];
                     foreach ($champs as $ch) {
                         $data = Input::post($ch);
                         if (null != $data && Count($data) > 0) {
-                            $repas->{$ch} = (in_array($unEnfant, $data)) ? 'Oui' : 'Non';
+                            $choix[$ch] = (in_array($unEnfant, $data)) ? 'Oui' : 'Non';
                         } else {
-                            $repas->{$ch} = "Non";
+                            $choix[$ch] = "Non";
                         }
                     }
-                    $repas->save();
-                    if ($repas->petitDej == 'Non' && $repas->dejeuner == 'Non' && $repas->gouter == 'Non' && $repas->absence == 'Non' && $repas->adhesion == 'Non') {
-                        $repas->delete();
+
+                    $repas = repasModel::findBy(array('nomEnfant=?', 'date=?'), array($unEnfant, $date));
+
+                    if (in_array('Oui', $choix)) {
+                        $existant = false;
+                        if (!$repas) {
+                            $repas            = new repasModel();
+                            $repas->tstamp    = time();
+                            $repas->date      = $date;
+                            $repas->nomEnfant = $unEnfant;
+                            $maj[]            = sprintf('Creation pour %s %s', $enf->nom, $enf->prenom);
+                        } else {
+                            $existant = true;
+                        }
+                        $i = 0;
+                        foreach ($choix as $key => $value) {
+                            if ($repas->{$key} != $value) {
+                                $i++;
+                                $repas->{$key} = $value;
+                            }
+                        }
+
+                        if ($i > 0 && $existant == true) {
+                            $maj[] = sprintf('Mise à jour pour %s %s', $enf->nom, $enf->prenom);
+                        }
+
+                        $repas->save();
+
+                    } else {
+                        if ($repas) {
+                            $repas->delete();
+                            $maj[] = sprintf('Suppression pour %s %s', $enf->nom, $enf->prenom);
+                        }
                     }
                 }
+                $jsonLog = [
+                    'maj' => sprintf('Mise a jour de la part de %s pour la date du %s', $this->User->username, Input::post('date')),
+                    'enf' => $maj,
+                ];
+                $this->sendLog(json_encode($jsonLog));
             }
         }
 
@@ -87,5 +121,14 @@ class beModuleSaisie extends \BackendModule
             }
             $this->Template->repas = $listeRepas;
         }
+    }
+
+    private function sendLog($texte)
+    {
+        $this->container  = System::getContainer();
+        $this->logger     = $this->container->get('monolog.logger.contao');
+        $this->projectDir = $this->container->getParameter('kernel.project_dir');
+
+        $this->logger->log(LogLevel::INFO, $texte, array('contao' => new ContaoContext('Cantine', "MAJ")));
     }
 }
